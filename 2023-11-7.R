86bf221771fc6c48e60472b719dc0f5d442cb7f9
fcosta00 <francescosta00@gmail.com>
2023-11-07 10:06:18 +0100

    Create 2023-11-7.R

---
+++ 2023-11-7.R
@@ -0,0 +1,17 @@
+# caricamento database
+library(MASS)
+data <- anorexia
+head( data )
+
+str( data )
+
+contrasts( data$Treat) # per vedere la tabella di contrasto
+
+data$Treat <- factor( data$Treat, levels = c('Cont', 'CBT', 'FT'))
+
+mod_cat<-lm( Postwt-Prewt ~ Treat, data=data)
+
+summary(mod_cat)
+# rteoricamnete l'intercetta è la media del gruppo di controllo
+
+with(data, tapply(Postwt-Prewt, Treat, mean) )
